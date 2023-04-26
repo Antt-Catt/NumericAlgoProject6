00c300133f059706b085f1898c216502ecd5270a
Arthur Le Floch <arthur.le_floch@enseirb-matmeca.fr>
2023-04-26 18:45:55 +0200

    Improved part2.py, added documentation

---
+++ src/part2.py
@@ -0,0 +1,108 @@
+from part1 import meth_n_step, step_rk4
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def first_models(y0, t0, tf, N, gamma, k):
+	"""
+	Function to plot a comparison between the first population model
+	and the Verhulst model.
+
+	Parameters
+	----------
+	y0 : float
+		Initial value of the population.
+	t0 : float
+		Initial time.
+	tf : float
+		Final time.
+	N : int
+		Number of steps.
+	gamma : float
+		Growth rate.
+	k : float
+		Carrying capacity.
+	"""
+	h = (tf - t0) / N
+	y = [t0 + h * i for i in range(N)]
+
+	# First differential equation
+	derivN1 = lambda x, t : gamma * x
+	res1 = meth_n_step(y0, t0, N, h, derivN1, step_rk4)
+	plt.subplot(1, 2, 1)
+	plt.title("Premier modèle de population")
+	plt.xlabel("Temps")
+	plt.ylabel("Nombre d'individus")
+	plt.plot(y, res1, marker='x')
+	plt.grid()
+
+	# Second differential equation
+	derivN2 = lambda x, t : gamma * x * (1 - x / k)
+	res2 = meth_n_step(y0, t0, N, h, derivN2, step_rk4)
+	plt.subplot(1, 2, 2)
+	plt.title("Modèle de population de Verhulst")
+	plt.xlabel("Temps")
+	plt.ylabel("Nombre d'individus")
+	plt.plot(y, res2, marker='x')
+	plt.grid()
+	plt.tight_layout()
+	plt.show()
+
+
+def second_model(y0, t0, tf, N, a, b, c, d):
+	"""
+	Function to plot the Lotka-Volterra model.
+
+	Parameters
+	----------
+	y0 : float
+		Initial value of the population.
+	t0 : float
+		Initial time.
+	tf : float
+		Final time.
+	N : int
+		Number of steps.
+	a : float
+		Growth rate of the predators.
+	b : float
+		Death rate of the predators.
+	c : float
+		Growth rate of the prey.
+	d : float
+		Death rate of the prey.
+	"""
+	h = (tf - t0) / N
+	y = [t0 + h * i for i in range(N)]
+
+	def derivative(Y, t):
+		return np.array([Y[0] * (a - b * Y[1]), Y[1] * (c * Y[0] - d)])
+	
+	res = meth_n_step(y0, t0, N, h, derivative, step_rk4)
+	plt.title("Modèle de Lotka-Volterra")
+	plt.xlabel("Temps")
+	plt.ylabel("Nombre d'individus")
+	plt.plot(y, res[:, 0], marker='x', label='Prédateurs')
+	plt.plot(y, res[:, 1], marker='x', label='Proies')
+	plt.grid()
+	plt.legend()
+	plt.tight_layout()
+	plt.show()
+
+
+if __name__ == '__main__':
+	gamma, k = 0.7, 6000
+
+	y0 = np.array([5000.])
+	N = 40
+	t0, tf = 0, 8
+
+	first_models(y0, t0, tf, N, gamma, k)
+
+	param = [1.5, 1, 1, 0.5]
+	y0 = np.array([1.5, 1])
+	N = 1000
+	t0, tf = 0, 100
+
+	second_model(y0, t0, tf, N, *param)
