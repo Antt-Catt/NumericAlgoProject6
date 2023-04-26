68b439b15aea1a85e24da6660d0ecc6efdd8b5f7
Arthur Le Floch <arthur.le_floch@enseirb-matmeca.fr>
2023-04-26 18:00:54 +0200

    Fixed part1.py

--- src/part1.py
+++ src/part1.py
@@ -1,29 +1,26 @@
 import numpy as np
 
 def step_euler(y, t, h, f):
-    return h * f(y, t)
+	return y + h * f(y, t)
 
-def step_middlepoint(y, t, h, f):
-    return h * f(y + (h/2) * f(y, t), t + h/2)
+def step_middle_point(y, t, h, f):
+	return y + h * f(y + h * f(y, t) / 2, t + h / 2)
 
-def step_Heun(y, t, h, f):
-    p1 = f(y, t)
-    p2 = f(y + h * p1, t + h)
-    return h * (p1 + p2) / 2
+def step_heun(y, t, h, f):
+	k1 = f(y, t)
+	k2 = f(y + h * k1, t + h)
+	return y + h * (k1 + k2) / 2
 
-def step_RK4(y, t, h, f):
-    p1 = f(y, t)
-    y2 = y + (h * p1) / 2
-    p2 = f(y2, t + h / 2)
-    y3 = y + (h * p2) / 2
-    p3 = f(y3, t + h / 2)
-    y4 = y + h * p3
-    p4 = f(y4, t + h)
+def step_rk4(y, t, h, f):
+	k1 = f(y, t)
+	k2 = f(y + h * k1 / 2, t + h / 2)
+	k3 = f(y + h * k2 / 2, t + h / 2)
+	k4 = f(y + h * k3, t + h)
+	return y + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
 def meth_n_step(y0, t0, N, h, f, meth):
-    y_array = np.full(N, y0)
-    tn = t0
-    for i in range(1, N):
-        tn += h
-        y_array[i] = meth(y_array[i - 1], tn, h, f)
-    return y_array
+	res = np.full(N, y0)
+	for i in range(1, N):
+		print(res[i - 1])
+		res[i] = meth(res[i - 1], t0 + i * h, h, f)
+	return res
