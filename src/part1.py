40ffa30fb4a90837642d456ddaa09b97c8fe1838
Arthur Le Floch <arthur.le_floch@enseirb-matmeca.fr>
2023-05-07 10:07:09 +0200

    Formatted code, added section 2 for report

--- src/part1.py
+++ src/part1.py
@@ -1,16 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def step_euler(y, t, h, f):
 	return y + h * f(y, t)
 
+
 def step_middle_point(y, t, h, f):
 	return y + h * f(y + h * f(y, t) / 2, t + h / 2)
+
 
 def step_heun(y, t, h, f):
 	k1 = f(y, t)
 	k2 = f(y + h * k1, t + h)
 	return y + h * (k1 + k2) / 2
+
 
 def step_rk4(y, t, h, f):
 	k1 = f(y, t)
@@ -19,12 +23,14 @@ def step_rk4(y, t, h, f):
 	k4 = f(y + h * k3, t + h)
 	return y + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
+
 def meth_n_step(y0, t0, N, h, f, meth):
 	res = np.zeros((N, len(y0)))
 	res[0] = y0.copy()
 	for i in range(1, N):
 		res[i] = meth(res[i - 1], t0 + i * h, h, f)
 	return res
+
 
 def meth_epsilon(y0, t0, tf, eps, f, meth):
 	N = 32
@@ -40,28 +46,31 @@ def meth_epsilon(y0, t0, tf, eps, f, meth):
 		y1 = y2
 	return y2
 
+
 def tangent_1D(f, ti, tf, yi, yf, step):
-    t = np.arange(ti, tf, step)
-    y = np.arange(yi, yf, step)
-    T, Y = np.meshgrid(t, y)
-    U = np.full(T.shape, 1)
-    V = f(T, Y)
-    plt.quiver(T, Y, U, V)
-    plt.xlabel('t')
-    plt.ylabel('y')
-    plt.show()
+	t = np.arange(ti, tf, step)
+	y = np.arange(yi, yf, step)
+	T, Y = np.meshgrid(t, y)
+	U = np.full(T.shape, 1)
+	V = f(T, Y)
+	plt.quiver(T, Y, U, V)
+	plt.xlabel('t')
+	plt.ylabel('y')
+	plt.show()
+
 
 def tangent_2D(f, ti, tf, yi, yf, step):
-    t = np.arange(ti, tf, step)
-    y1 = np.arange(yi, yf, step)
-    y2 = np.arange(yi, yf, step)
-    Y1, Y2 = np.meshgrid(y1, y2)
-    U = -Y2
-    V = Y1
-    plt.quiver(Y1, Y2, U, V)
-    plt.xlabel('y1')
-    plt.ylabel('y2')
-    plt.show()
+	t = np.arange(ti, tf, step)
+	y1 = np.arange(yi, yf, step)
+	y2 = np.arange(yi, yf, step)
+	Y1, Y2 = np.meshgrid(y1, y2)
+	U = -Y2
+	V = Y1
+	plt.quiver(Y1, Y2, U, V)
+	plt.xlabel('y1')
+	plt.ylabel('y2')
+	plt.show()
+
 
 if __name__ == '__main__':
 	f = lambda t, y: y / (1 + t * t)
